fb9f80fe98d1b6e8bfc0e7dd078258cc6ba24443
Dmitry Anisov <anis.dim@gmail.com>
2022-03-30 14:15:17 +0300

    minor router fixes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const stats = require("./routes/stats");
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "/client/build")));
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 app.use(express.json());
