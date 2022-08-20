1ce5c588775c55fcfc10d8ae2089647287643546
Dmitry Anisov <anis.dim@gmail.com>
2022-08-20 13:50:09 +0300

    update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
-  "http://http://192.168.31.38:5000",
+  "http://192.168.31.38:5000",
   "http://localhost:3000",
   "http://192.168.31.38:3000",
+  "https://tg-workout-tracker-dashboard.herokuapp.com",
 ];
 
 module.exports = allowedOrigins;
