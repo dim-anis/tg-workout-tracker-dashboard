6e4144f4fa941b2a98349eafc8f64837705b6805
Dmitry Anisov <anis.dim@gmail.com>
2022-08-21 17:35:15 +0300

    update serve react

--- server.js
+++ server.js
@@ -38,9 +38,9 @@ if (
   process.env.NODE_ENV === "production" ||
   process.env.NODE_ENV === "staging"
 ) {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
